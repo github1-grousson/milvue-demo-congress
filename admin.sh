b653deb5da3b330ef7732fb0d48fa8b1857763dc
Wilfried Grousson <wilfried@milvue.com>
2025-02-24 15:38:53 +0100

    feat(docker): use new image with log level and user

--- admin.sh
+++ admin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-docker run --name milvue-admin-tool --rm -it --network=host -v "$DIR/assets:/app/assets" -v "$DIR/templates:/app/templates" wilfriedmv/milvue-admin-tool:latest
+docker run --name milvue-admin-tool --rm -it --network=host -e LOG_LEVEL=INFO -v /etc/localtime:/etc/localtime:ro -v "$DIR/assets:/app/assets" -v "$DIR/templates:/app/templates" -u $(id -u):$(id -g) wilfriedmv/milvue-admin-tool:latest
